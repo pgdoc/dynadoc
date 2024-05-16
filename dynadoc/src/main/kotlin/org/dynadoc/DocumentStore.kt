61474187c5a5a671d8ee17702ce9068f9d14baba
Flavien Charlon <flavien@charlon.org>
2024-05-16 23:04:41 +0100

    Create the DocumentStore interface

---
+++ dynadoc/src/main/kotlin/org/dynadoc/DocumentStore.kt
@@ -0,0 +1,26 @@
+package org.dynadoc
+
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
+import java.util.*
+
+interface DocumentStore {
+    @Throws(UpdateConflictException::class)
+    suspend fun updateDocuments(updatedDocuments: Iterable<Document>, checkedDocuments: Iterable<Document>)
+
+    fun getDocuments(ids: Iterable<DocumentKey>): Flow<Document>
+}
+
+class UpdateConflictException(
+    val id: DocumentKey
+) : RuntimeException("The object $id has been modified.")
+
+
+@Throws(UpdateConflictException::class)
+suspend fun DocumentStore.updateDocuments(vararg documents: Document) {
+    updateDocuments(documents.toList(), listOf())
+}
+
+suspend fun DocumentStore.getDocument(id: DocumentKey): Document {
+    return getDocuments(listOf(id)).first()
+}
